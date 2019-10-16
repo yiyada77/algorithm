b70f9caeef3f5332500b99573aaab8920ce592df
Yu Shixin <yiyada@vip.qq.com>
2019-10-16 23:09:42 +0800

    array practice

---
+++ 448-Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,13 @@
+var findDisappearedNumbers = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        nums[index] = -Math.abs(nums[index]);
+    }
+    res = [];
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] > 0) {
+            res.push(j + 1);
+        }
+    }
+    return res;
+};
