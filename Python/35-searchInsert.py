a0c1c67f0cec4123435d43b3062409ec39ef6f17
Yu Shixin <yiyada@vip.qq.com>
2021-09-21 22:32:17 +0800

    practice python 2

---
+++ Python/35-searchInsert.py
@@ -0,0 +1,11 @@
+def searchInsert(self, nums, target):
+    start, end = 0, len(nums) - 1
+    while end >= start:
+        mid = (end + start) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return start
